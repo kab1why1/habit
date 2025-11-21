4b1a02fa840b67c224b43a3fc1d31a4b3a1c0991
kab1 <kab1why@gmail.com>
2025-11-21 18:33:51 +0200

    added layout for a habit pages

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
-
 const { testConnection } = require('./server/config/db');
-const { createHabitTable } = require('./server/models/habit');
+const { createHabitsTable } = require('./server/models/habit');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Public
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// EJS
+// Views
 app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
@@ -21,11 +21,10 @@ app.set('view engine', 'ejs');
 // Routes
 app.use('/', require('./server/routes/main'));
 
-// Start Server
+// Server
 app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
-
   await testConnection();
-  await createHabitTable();
-  console.log("Habit table ready");
+  await createHabitsTable();
+  console.log("Habits table ready");
 });
